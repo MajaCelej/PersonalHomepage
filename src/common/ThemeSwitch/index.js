6f63b53679928e51978bf06dcfd5254304fffe66
Maja Celej <cmaja1825@gmail.com>
2024-04-16 09:19:56 +0200

    deleted unused code

--- src/common/ThemeSwitch/index.js
+++ src/common/ThemeSwitch/index.js
@@ -1,18 +1,13 @@
-import { useDispatch, useSelector } from "react-redux";
-import { selectIsDarkTheme, toggleTheme } from "../themeSlice";
 import { Wrapper, Button, Text, Box, IconWrapper } from "./styled.js";
 
 export const ThemeSwitch = () => {
-	const isDarkTheme = useSelector(selectIsDarkTheme);
-	const dispatch = useDispatch();
 
 	return (
 		<Wrapper>
-			<Button onClick={() => dispatch(toggleTheme())}>
-				<Text>Dark Mode {isDarkTheme ? "on" : "off"} </Text>
+		    <Button >
+				<Text> Mode </Text>
 				<Box>
-					<IconWrapper moveToRight={isDarkTheme}>
-						<Icon />
+					<IconWrapper>
 					</IconWrapper>
 				</Box>
 			</Button>
